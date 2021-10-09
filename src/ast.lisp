1185284097bf0beadd952bd9da78f74436a3a12c
Kartik Singh <kartiksingh_ma@yahoo.com>
2021-10-09 14:34:58 -0700

    Add initial AST definitions

---
+++ src/ast.lisp
@@ -0,0 +1,52 @@
+;;;; AST node definitions
+
+(in-package :strl2v)
+
+(defstruct ast-node
+  source-file
+  source-pos)
+
+;; top-level program node
+(defstruct (program (:include ast-node) (:copier nil))
+  modules)
+
+;; expressions
+(defstruct (signal-name (:include ast-node) (:copier nil))
+  name)
+
+;; statements
+(defstruct (module-def (:include ast-node) (:copier nil))
+  name
+  inputs
+  outputs
+  body)
+(defstruct (nothing-stmt (:include ast-node) (:copier nil)))
+(defstruct (pause-stmt (:include ast-node) (:copier nil)))
+(defstruct (seq-stmt (:include ast-node) (:copier nil))
+  statements)
+(defstruct (par-stmt (:include ast-node) (:copier nil))
+  branches)
+(defstruct (loop-stmt (:include ast-node) (:copier nil))
+  body)
+(defstruct (loop-each-stmt (:include ast-node) (:copier nil))
+  signal
+  body)
+(defstruct (sig-decl-stmt (:include ast-node) (:copier nil))
+  signals
+  body)
+(defstruct (emit-stmt (:include ast-node) (:copier nil))
+  signals)
+(defstruct (present-stmt (:include ast-node) (:copier nil))
+  signal
+  then-body
+  else-body)
+(defstruct (suspend-stmt (:include ast-node) (:copier nil))
+  signal
+  body)
+(defstruct (trap-stmt (:include ast-node) (:copier nil))
+  label
+  body)
+(defstruct (exit-stmt (:include ast-node) (:copier nil))
+  label)
+(defstruct (await-stmt (:include ast-node) (:copier nil))
+  signal)
